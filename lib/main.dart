24e848367354b5c87e55a99ecbd92ccacc5548b8
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-06-12 19:27:57 +0530

    stateful widget basic stuct

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 92, 192, 173),
-                Color.fromARGB(255, 1, 108, 88),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 92, 192, 173),
+                Color.fromARGB(255, 1, 108, 88),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
